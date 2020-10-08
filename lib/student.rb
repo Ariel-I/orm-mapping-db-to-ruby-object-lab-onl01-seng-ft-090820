74a1ba69300a69360277cb866deca757a69a8aa2
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-08 02:28:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
      end
   end 
   
-  def self .first_student_in_grade_10
+  def self .first_student_in_grade_10(id)
     sql = <<-SQL
     SELECT *
     FROM students
@@ -78,7 +78,7 @@ class Student
     LIMIT 1
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, id)
     
   end 
   
