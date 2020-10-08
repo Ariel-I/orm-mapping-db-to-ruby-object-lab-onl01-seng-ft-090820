db46821382b9c37acbf7fce7d3684bc31712c6d4
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-08 02:31:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,12 +71,17 @@ class Student
   end 
   
   def self .first_student_in_grade_10(id)
+     
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1
+    SQL
     
-    s = self.first_X_students_in_grade_10(1)
-    s.map do |i|
-      self.new_from_db(i)
-    end[0]
-   
+    DB[:conn].execute(sql, id).collect do |row|
+      self.new_from_db(row)
+    end
     
   end 
   
