9f0f7139b0e498eb27ccaa105bd79e3375775eba
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-08 01:46:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,12 +49,10 @@ class Student
     sql = <<-SQL
     SELECT *
     FROM students 
-    WHERE grade <= 12
+    WHERE grade < 10
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end 
+    DB[:conn].execute(sql)
     
   end 
   
